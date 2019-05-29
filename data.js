36501e571e6c47bfdc7e88d9c56e57f752678fe9
Maxine Hartnett <maxinehartnett@gmail.com>
2019-05-29 16:57:44 -0600

    I am a god at js, if by 'god' you mean 'really bad but doesn't care that much so it works but barely'

---
+++ data.js
@@ -0,0 +1,6 @@
+// define some sample data
+var tabledata = [
+    {id:1, name:"Zirconite", carat:"2", col:"red", price:"20"},
+    {id:2, name:"Quartz", carat:"20", col:"white", price:"2"},
+    {id:3, name:"Quartz", carat:"5", col:"green", price:"1.5"}
+];
