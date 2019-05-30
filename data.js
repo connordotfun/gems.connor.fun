757a9645d75a2ea6f7adaad3a08239fc9d6d06fc
Maxine Hartnett <maxinehartnett@gmail.com>
2019-05-29 19:04:56 -0600

    Added real data

--- data.js
+++ data.js
@@ -1,7 +1,939 @@
 // whee, js is fun
-// Here's the data! This stuff is fake... for now
 var tabledata = [
-    {id:1, name:"Zirconite", carat:"2", col:"red", price:"20"},
-    {id:2, name:"Quartz", carat:"20", col:"white", price:"2"},
-    {id:3, name:"Quartz", carat:"5", col:"green", price:"1.5"}
-];
+    {
+      "type": "Rhodolite Garnet",
+      "size": "",
+      "color": "",
+      "carats": "",
+      "shape": "",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Ruby",
+      "size": "7x5.2x4.66",
+      "color": "Pink Red",
+      "carats": 1.47,
+      "shape": "Oval",
+      "price": 3,
+      "origin": "Mozambique"
+    },
+    {
+      "type": "Ruby",
+      "size": "7x5x4.59",
+      "color": "Pink Red",
+      "carats": 1.45,
+      "shape": "Oval",
+      "price": 3,
+      "origin": "Mozambique"
+    },
+    {
+      "type": "Ruby",
+      "size": "7x5.2x4.66",
+      "color": "Pink Red",
+      "carats": 1.47,
+      "shape": "Oval",
+      "price": 3,
+      "origin": "Mozambique"
+    },
+    {
+      "type": "Ruby",
+      "size": "7x5.2x4.66",
+      "color": "Pink Red",
+      "carats": 1.47,
+      "shape": "Oval",
+      "price": 3,
+      "origin": "Mozambique"
+    },
+    {
+      "type": "Ruby",
+      "size": "8x6x4.5",
+      "color": "Pink Red",
+      "carats": 2,
+      "shape": "Oval",
+      "price": "",
+      "origin": "Mozambique"
+    },
+    {
+      "type": "Hessionite Garnet",
+      "size": 6,
+      "color": "Orange",
+      "carats": 1.41,
+      "shape": "Round",
+      "price": 15,
+      "origin": "Madagascar"
+    },
+    {
+      "type": "Hessionite Garnet",
+      "size": 7,
+      "color": "Orange Red",
+      "carats": 1.68,
+      "shape": "Round",
+      "price": 18,
+      "origin": "Madagascar"
+    },
+    {
+      "type": "Kyanite",
+      "size": "7x5",
+      "color": "Blue",
+      "carats": 1.14,
+      "shape": "Oval",
+      "price": 6,
+      "origin": "Sri Lanka"
+    },
+    {
+      "type": "Kyanite",
+      "size": "7x5",
+      "color": "Blue",
+      "carats": 1.16,
+      "shape": "Oval",
+      "price": 6,
+      "origin": "Sri Lanka"
+    },
+    {
+      "type": "Kyanite",
+      "size": "8x6x4.5",
+      "color": "Blue",
+      "carats": 1.52,
+      "shape": "Oval",
+      "price": 6,
+      "origin": "Nepal"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "6.2x5x3.4",
+      "color": "Orange",
+      "carats": 1.01,
+      "shape": "Oval",
+      "price": 30,
+      "origin": "Tanzania"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "7x5",
+      "color": "Orange",
+      "carats": 0.92,
+      "shape": "Trillion",
+      "price": 17,
+      "origin": "Tanzania"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "6.1x5.2x3.5",
+      "color": "Orange",
+      "carats": 1.06,
+      "shape": "Cushion",
+      "price": 16,
+      "origin": "Nigeria"
+    },
+    {
+      "type": "Indicolite",
+      "size": "8.4x6.1x4.2",
+      "color": "Blue Green",
+      "carats": 2.03,
+      "shape": "Emerald",
+      "price": 48,
+      "origin": "Tanzania"
+    },
+    {
+      "type": "Spinel",
+      "size": "5.6 x 4.5 x 2.9",
+      "color": "Pink Purple",
+      "carats": 0.43,
+      "shape": "Oval",
+      "price": 11.5,
+      "origin": "Tanzania"
+    },
+    {
+      "type": "Rhodonite",
+      "size": "10.92 x 8.04 x 6.39",
+      "color": "Red",
+      "carats": 5.43,
+      "shape": "Emerald",
+      "price": 22,
+      "origin": "Brazil"
+    },
+    {
+      "type": "Tanzanite",
+      "size": "5.07 x 5.05 x 3.69",
+      "color": "Blue Violet",
+      "carats": 0.76,
+      "shape": "Cushion",
+      "price": 12,
+      "origin": "Tanzania"
+    },
+    {
+      "type": "Chrysoberyl",
+      "size": "7x5x3",
+      "color": "Yellow",
+      "carats": 1.25,
+      "shape": "Oval",
+      "price": 30,
+      "origin": "Sri Lanka"
+    },
+    {
+      "type": "Chrysoberyl",
+      "size": "6x4x2.5",
+      "color": "Yellow",
+      "carats": 0.65,
+      "shape": "Oval",
+      "price": 15,
+      "origin": "Sri Lanka"
+    },
+    {
+      "type": "Golden Beryl",
+      "size": "7x6x4",
+      "color": "Yellow",
+      "carats": 1.12,
+      "shape": "Oval",
+      "price": 4,
+      "origin": "Brazil"
+    },
+    {
+      "type": "Golden Beryl",
+      "size": "7.2x6.2x4.4",
+      "color": "Yellow",
+      "carats": 1.35,
+      "shape": "Cushion",
+      "price": 5,
+      "origin": "Brazil"
+    },
+    {
+      "type": "Rubellite",
+      "size": "8x5.6x4",
+      "color": "Pink",
+      "carats": 1.2,
+      "shape": "Oval",
+      "price": 15,
+      "origin": "Mozambique"
+    },
+    {
+      "type": "Tourmaline",
+      "size": "10.3x5.5x3",
+      "color": "Green",
+      "carats": 1.67,
+      "shape": "Emerald",
+      "price": 7,
+      "origin": "Nigeria"
+    },
+    {
+      "type": "Tourmaline",
+      "size": "7.2x5.2x3.1",
+      "color": "Watermelon",
+      "carats": 1.18,
+      "shape": "Emerald",
+      "price": "",
+      "origin": "Brazil"
+    },
+    {
+      "type": "Tourmaline",
+      "size": "10.2x5.3x3.6",
+      "color": "Green",
+      "carats": 1.83,
+      "shape": "Emerald",
+      "price": 13,
+      "origin": "Mozambique"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "4.8x4.8",
+      "color": "Orange",
+      "carats": 0.8,
+      "shape": "Trillion",
+      "price": 5,
+      "origin": "Namibia"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "4x4",
+      "color": "Orange",
+      "carats": 0.8,
+      "shape": "Square",
+      "price": 5,
+      "origin": "Namibia"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "5x4.4",
+      "color": "Orange",
+      "carats": 0.8,
+      "shape": "Heart",
+      "price": 5,
+      "origin": "Namibia"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "4.3x4.9 to 4.9x5.3",
+      "color": "Orange",
+      "carats": 0.4,
+      "shape": "Trillion",
+      "price": 13.5,
+      "origin": "Namibia"
+    },
+    {
+      "type": "Garnet",
+      "size": "6.x6x3.8",
+      "color": "Red",
+      "carats": 1.01,
+      "shape": "Heart",
+      "price": 2,
+      "origin": "Africa"
+    },
+    {
+      "type": "Garnet",
+      "size": "6.x6x3.8",
+      "color": "Red",
+      "carats": 1.01,
+      "shape": "Heart",
+      "price": 2,
+      "origin": "Africa"
+    },
+    {
+      "type": "Garnet",
+      "size": "6.x6x3.8",
+      "color": "Red",
+      "carats": 1.01,
+      "shape": "Heart",
+      "price": 2,
+      "origin": "Africa"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "5.7x5x3.8",
+      "color": "Red",
+      "carats": 1.03,
+      "shape": "Cushion",
+      "price": 13,
+      "origin": "Africa"
+    },
+    {
+      "type": "Rhodolite Garnet",
+      "size": "7.2x5.9x3.5",
+      "color": "Pink",
+      "carats": 1.11,
+      "shape": "Pear",
+      "price": 5,
+      "origin": "Africa"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "5.4x3.9",
+      "color": "Orange Red",
+      "carats": 1.03,
+      "shape": "Round",
+      "price": 7,
+      "origin": "Africa"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "6.2x6.1x2.8",
+      "color": "Orange",
+      "carats": 0.82,
+      "shape": "Trillion",
+      "price": 9,
+      "origin": "Africa"
+    },
+    {
+      "type": "Garnet",
+      "size": "6.5x4.8x3",
+      "color": "Red",
+      "carats": 0.74,
+      "shape": "Pear",
+      "price": 6,
+      "origin": "Africa"
+    },
+    {
+      "type": "Tourmaline",
+      "size": "6.5x4.1x3",
+      "color": "Green",
+      "carats": 0.74,
+      "shape": "Emerald",
+      "price": 2,
+      "origin": "Mozambique"
+    },
+    {
+      "type": "Tanzanite",
+      "size": "6.3x6.3",
+      "color": "Blue Green",
+      "carats": 0.76,
+      "shape": "Trillion",
+      "price": 24,
+      "origin": "Tanzania"
+    },
+    {
+      "type": "Garnet",
+      "size": "5.6x2.6",
+      "color": "Red",
+      "carats": 0.7,
+      "shape": "Round",
+      "price": 2,
+      "origin": "Africa"
+    },
+    {
+      "type": "Rhodolite Garnet",
+      "size": "7.7x5.9x3.2",
+      "color": "Pink",
+      "carats": 1.29,
+      "shape": "Oval",
+      "price": 5,
+      "origin": "Africa"
+    },
+    {
+      "type": "Welo Opal",
+      "size": "6.6x6.6",
+      "color": "Multi",
+      "carats": 0.6,
+      "shape": "Trillion",
+      "price": 13,
+      "origin": "Ethiopia"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "6.2x5.5x3.5",
+      "color": "Red Orange",
+      "carats": 1.13,
+      "shape": "Oval",
+      "price": 4,
+      "origin": "Africa"
+    },
+    {
+      "type": "Tourmaline",
+      "size": "12.7x4.3x3.2",
+      "color": "Green Blue",
+      "carats": 1.59,
+      "shape": "Emerald",
+      "price": 10,
+      "origin": "Africa"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "6.1x5.2x3.5",
+      "color": "Orange",
+      "carats": 1.06,
+      "shape": "Oval",
+      "price": 20,
+      "origin": "Tanzania"
+    },
+    {
+      "type": "Tourmaline",
+      "size": "8.5x5.5x3.4",
+      "color": "Green",
+      "carats": 1.38,
+      "shape": "Emerald",
+      "price": "",
+      "origin": "Africa"
+    },
+    {
+      "type": "Rhodolite Garnet",
+      "size": ".9x6.4x4.7",
+      "color": "Pink Red",
+      "carats": 2.09,
+      "shape": "Oval",
+      "price": 14,
+      "origin": "Africa"
+    },
+    {
+      "type": "Spessartite Garnet",
+      "size": "4.5x4.5x2.5",
+      "color": "Orange",
+      "carats": ".6 ea",
+      "shape": "Square",
+      "price": 5,
+      "origin": "Namibia"
+    },
+    {
+      "type": "Emerald",
+      "size": "9 x 8.5",
+      "color": "muzo",
+      "carats": 3.13,
+      "shape": "Cushion",
+      "price": 29,
+      "origin": ""
+    },
+    {
+      "type": "Sapphire",
+      "size": 3,
+      "color": "multi",
+      "carats": 0.4,
+      "shape": "round",
+      "price": 0.7,
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": 3,
+      "color": "multi",
+      "carats": 0.25,
+      "shape": "Round",
+      "price": 0.7,
+      "origin": ""
+    },
+    {
+      "type": "Zircon",
+      "size": 4,
+      "color": "orange",
+      "carats": 0.3,
+      "shape": "Round",
+      "price": 0.3,
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": "5 x 4",
+      "color": "green mint",
+      "carats": 1.05,
+      "shape": "Octagon",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Spinel",
+      "size": "5 x 4",
+      "color": "red",
+      "carats": 0.5,
+      "shape": "Oval",
+      "price": 4,
+      "origin": ""
+    },
+    {
+      "type": "Spinel",
+      "size": "5 x 4",
+      "color": "pink",
+      "carats": 0.53,
+      "shape": "Oval",
+      "price": 6,
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": "5.9 x 5.7",
+      "color": "orange",
+      "carats": 1.29,
+      "shape": "Cushion",
+      "price": 26,
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": 6,
+      "color": "orange",
+      "carats": 1.41,
+      "shape": "Round",
+      "price": 14,
+      "origin": ""
+    },
+    {
+      "type": "Tsavorite",
+      "size": "6.1 x 4.5",
+      "color": "green",
+      "carats": 0.67,
+      "shape": "Octagon",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": "6.3 x 5",
+      "color": "orange",
+      "carats": 0.85,
+      "shape": "oval",
+      "price": 2,
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": "6.33 x 6.27",
+      "color": "red",
+      "carats": 1.35,
+      "shape": "oval",
+      "price": 10,
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": "6.44 x 4.8",
+      "color": "pink",
+      "carats": 1.08,
+      "shape": "oval",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Spinel",
+      "size": "6.5 x 5.2",
+      "color": "blue",
+      "carats": 1.06,
+      "shape": "oval",
+      "price": 20,
+      "origin": ""
+    },
+    {
+      "type": "Spinel",
+      "size": "6.5 x 5.2",
+      "color": "pink",
+      "carats": 1.08,
+      "shape": "Oval",
+      "price": 39,
+      "origin": ""
+    },
+    {
+      "type": "Spinel",
+      "size": "6.6 x 6",
+      "color": "pink",
+      "carats": 1.19,
+      "shape": "Cushion",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": "6.73 x 6.44",
+      "color": "green mint",
+      "carats": 1.92,
+      "shape": "Octagon",
+      "price": 50,
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": "6.8 x 5.6",
+      "color": "pink",
+      "carats": 0.82,
+      "shape": "Oval",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": "6.94 x 6",
+      "color": "green blue",
+      "carats": 1.64,
+      "shape": "Octagon",
+      "price": 70,
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": "7 x 4.5",
+      "color": "green mint",
+      "carats": 0.6,
+      "shape": "Octagon",
+      "price": 10,
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": "7 x 6",
+      "color": "red",
+      "carats": 1.44,
+      "shape": "Oval",
+      "price": 1,
+      "origin": ""
+    },
+    {
+      "type": "Ruby",
+      "size": 7,
+      "color": "red",
+      "carats": 1.1,
+      "shape": "Oval",
+      "price": 5,
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": 7,
+      "color": "orange",
+      "carats": 1.68,
+      "shape": "Round",
+      "price": 15,
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": 7,
+      "color": "orange",
+      "carats": 0.92,
+      "shape": "trillion",
+      "price": 17,
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": 7.1,
+      "color": "yellow",
+      "carats": 0.84,
+      "shape": "oval",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": 7.2,
+      "color": "orange",
+      "carats": 1.19,
+      "shape": "oval",
+      "price": 31,
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": 7.8,
+      "color": "green",
+      "carats": 1.18,
+      "shape": "Octagon",
+      "price": 12,
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": 7.89,
+      "color": "red",
+      "carats": 1.67,
+      "shape": "trillion",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Kyanite",
+      "size": "8 x 6",
+      "color": "blue",
+      "carats": 1.5,
+      "shape": "Oval",
+      "price": 8.5,
+      "origin": ""
+    },
+    {
+      "type": "Zircon",
+      "size": "8 x 6",
+      "color": "blue",
+      "carats": 2.09,
+      "shape": "oval",
+      "price": 25,
+      "origin": ""
+    },
+    {
+      "type": "Iolite",
+      "size": "8.07 x 6",
+      "color": "blue",
+      "carats": 1,
+      "shape": "Oval",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": "8.1 x 6",
+      "color": "red",
+      "carats": 1.75,
+      "shape": "Oval",
+      "price": 2,
+      "origin": ""
+    },
+    {
+      "type": "Zircon",
+      "size": "8.2 x 7",
+      "color": "orange",
+      "carats": 2.6,
+      "shape": "oval",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Zircon",
+      "size": "8.2 x 7",
+      "color": "blue",
+      "carats": 3.2,
+      "shape": "Oval",
+      "price": 17,
+      "origin": ""
+    },
+    {
+      "type": "Apatite",
+      "size": "8.4 x 6",
+      "color": "turquoise",
+      "carats": 1.32,
+      "shape": "pear",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": "8.63 x 6.42",
+      "color": "indicolite",
+      "carats": 2.6,
+      "shape": "Octagon",
+      "price": 64,
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": 8.7,
+      "color": "indicolite",
+      "carats": 1.22,
+      "shape": "Octagon",
+      "price": 8,
+      "origin": ""
+    },
+    {
+      "type": "Spinel",
+      "size": 8.8,
+      "color": "pink",
+      "carats": 1.76,
+      "shape": "pear",
+      "price": 30,
+      "origin": ""
+    },
+    {
+      "type": "Zircon",
+      "size": "8.8 x 7",
+      "color": "orange",
+      "carats": 2.37,
+      "shape": "pear",
+      "price": 6,
+      "origin": ""
+    },
+    {
+      "type": "Kyanite",
+      "size": "9 x 7",
+      "color": "blue",
+      "carats": 2.11,
+      "shape": "oval",
+      "price": 17,
+      "origin": ""
+    },
+    {
+      "type": "Kyanite",
+      "size": "9 x 7",
+      "color": "blue",
+      "carats": 2.18,
+      "shape": "Oval",
+      "price": 19,
+      "origin": ""
+    },
+    {
+      "type": "Kyanite",
+      "size": "9.18 x 7",
+      "color": "blue",
+      "carats": 2.52,
+      "shape": "Oval",
+      "price": 6,
+      "origin": ""
+    },
+    {
+      "type": "Kyanite",
+      "size": "9.3 x 6.5",
+      "color": "blue",
+      "carats": 2.36,
+      "shape": "oval",
+      "price": 1,
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": "9.5 x 8.2",
+      "color": "olive green",
+      "carats": 4.9,
+      "shape": "Octagon",
+      "price": 22,
+      "origin": ""
+    },
+    {
+      "type": "Quartz",
+      "size": "9.93 x 8",
+      "color": "yellow",
+      "carats": 3.33,
+      "shape": "octagon",
+      "price": 2,
+      "origin": ""
+    },
+    {
+      "type": "Tourmaline",
+      "size": "10 x 4.6",
+      "color": "indicolite",
+      "carats": 1.1,
+      "shape": "",
+      "price": 10,
+      "origin": ""
+    },
+    {
+      "type": "Sphene",
+      "size": "10.7 x 7.5",
+      "color": "golden",
+      "carats": 3.4,
+      "shape": "oval",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Kyanite",
+      "size": "11.4 x 8.3",
+      "color": "blue",
+      "carats": 4.08,
+      "shape": "oval",
+      "price": 15,
+      "origin": ""
+    },
+    {
+      "type": "Kyanite",
+      "size": "11.5 x 6.4",
+      "color": "orange",
+      "carats": 3.09,
+      "shape": "octagon",
+      "price": 44,
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": "12 x 6",
+      "color": "red",
+      "carats": 1.72,
+      "shape": "marquise",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Sapphire",
+      "size": "12 x 10.4",
+      "color": "blue",
+      "carats": 5.56,
+      "shape": "Oval",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Kyanite",
+      "size": "12.4 x 8",
+      "color": "blue",
+      "carats": 4.85,
+      "shape": "oval",
+      "price": 5,
+      "origin": ""
+    },
+    {
+      "type": "Kyanite",
+      "size": "12.5  x 8.9",
+      "color": "blue",
+      "carats": 3.7,
+      "shape": "oval",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Kyanite",
+      "size": "12.8 x 6.8",
+      "color": "blue",
+      "carats": 3.6,
+      "shape": "oval",
+      "price": "",
+      "origin": ""
+    },
+    {
+      "type": "Garnet",
+      "size": "14.3 x 11.7",
+      "color": "red",
+      "carats": 8.8,
+      "shape": "Oval",
+      "price": 5,
+      "origin": ""
+    }
+  ]
